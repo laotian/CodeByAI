5193d2209e88876aad71a842492867315709c4c1
田鲁攀 <tianlp@rrxplatform.com>
2020-07-11 14:25:16 +0800

    support customize

--- CodeByAI.sketchplugin/Contents/Sketch/library/generateService.sh
+++ CodeByAI.sketchplugin/Contents/Sketch/library/generateService.sh
@@ -9,12 +9,16 @@ fi
 if [ -d "CodeByAI" ]; then
   rm -rf CodeByAI
 fi
+if [ ! -f "index.html" ]; then
+  cp index.html index.html.bak
+fi
 zip -r sketch.zip  assets/*@2x.png artboard.json *.html
 curl $apiUrl -X POST -F "sketchZip=@sketch.zip" -o CodeByAI.zip
 unzip CodeByAI.zip
 cp CodeByAI/index.html ./
+rm CodeByAI/index.html
 rm CodeByAI.zip
 rm sketch.zip
-if [ -f "/usr/local/bin/codebyai-cli" ]; then
-  /usr/local/bin/codebyai-cli "$sketchDir"
-fi
+#if [ -f "/usr/local/bin/codebyai-cli" ]; then
+#  /usr/local/bin/codebyai-cli "$sketchDir"
+#fi
